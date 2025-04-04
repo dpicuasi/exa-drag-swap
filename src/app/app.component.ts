2e8b1b831bf14c7929a5af60f9523c79a87283df
dpicuasi <danyeledg@gmail.com>
2025-04-03 23:51:34 -0500

    Clean app component file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,26 +13,11 @@ interface Item {
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements AfterViewInit, OnDestroy {
+export class AppComponent implements AfterViewInit {
 
   @ViewChild('containerRef') containerRef!: ElementRef<HTMLDivElement>;
 
-  items: Item[] = [
-    { id: '1', title: '1' , slot:'1'},
-    { id: '2', title: '2', slot:'2'},
-    { id: '3', title: '3' , slot:'3'},
-    { id: '4', title: '4' , slot:'4'},
-  ]
-
-  slotItemMap: SlotItemMapArray = utils.initSlotItemMap(this.items, 'id');
-  slottedItems: any[] = [];
-  swapyRef: Swapy | null = null;
-  idCounter = 5;
-
   constructor() {
-    this.slottedItems = utils.toSlottedItems(this.items, 'id', this.slotItemMap);
-    console.log('slottedItems', this.slottedItems);
-    console.log('slotItemMap', this.slotItemMap);
   }
 
   ngAfterViewInit(): void {
@@ -40,48 +25,5 @@ export class AppComponent implements AfterViewInit, OnDestroy {
     swapy.onSwap((event) => console.log('event', event));
   }
 
-  updateSlottedItems() {
-    this.slottedItems = utils.toSlottedItems(
-      this.items,
-      'id',
-      this.slotItemMap
-    )
-  }
-
-  addItem() {
-    const newItem: Item = { id: `${this.idCounter}`, title: `${this.idCounter}`, slot: `${this.idCounter}` }
-    this.items = [...this.items, newItem]
-    this.slotItemMap = utils.initSlotItemMap(this.items, 'id')
-    this.updateSlottedItems()
-    this.idCounter++
-  }
-
-  removeItem(id: string) {
-    this.items = this.items.filter((i) => i.id !== id)
-    this.slotItemMap = utils.initSlotItemMap(this.items, 'id')
-    this.updateSlottedItems()
-  }
-
-  ngOnDestroy(): void {
-    this.swapyRef?.destroy()
-  }
-
-  handleKeyDown(event: KeyboardEvent, itemId: string) {
-    // Check if the pressed key is Enter  
-    if (event.key === 'Enter') {
-      const target = event.target as HTMLInputElement
-      const itemId = target.getAttribute('data-item-id')
-      if (itemId) {
-        this.removeItem(itemId)
-      }
-    }
-  }
-
-  handleAddItemKeyDown(event: KeyboardEvent) {
-    // Check if the pressed key is Enter  
-    if (event.key === 'Enter') {
-      this.addItem()
-    }
-  }
   
 }
